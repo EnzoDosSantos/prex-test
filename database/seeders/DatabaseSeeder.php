ce352d6b56810c833dd6b5fe02844a93b22350aa
Enzo Facundo Dos Santos <enzodossantos364@gmail.com>
2024-03-07 17:05:06 -0300

    chore(config): Add migration and sedeer for Gifts Model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Gifts;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Faker\Factory as Faker;
@@ -15,13 +16,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->seedUserTable();
+        $this->seedGiftTable();
     }
 
     private function seedUserTable(): void
     {
         $faker = Faker::create();
 
-        foreach (range(1, 5) as $idx) {
+        foreach(range(1, 5) as $idx) {
             DB::table('prex_user')->insert([
                 'email' => $faker->unique()->safeEmail,
                 'password' => Hash::make('password'),
@@ -32,5 +34,26 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@test.com',
             'password' => Hash::make('password'),
         ]);
+    }
+
+    private function seedGiftTable(): void
+    {
+        $faker = Faker::create('es_ES');
+
+        $gifts = [];
+
+        foreach(range(1, 50) as $idx){
+
+            $toInsertGift = [];
+
+            $toInsertGift['external_id'] = $faker->uuid;
+            $toInsertGift['title'] = $faker->realText;
+            $toInsertGift['url'] = $faker->url();
+
+            $gifts[] = $toInsertGift;
+
+        }
+
+        Gifts::insert($gifts);
     }
 }
