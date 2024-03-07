4d64619a90faefec076ae746a17400cfd4ac3e6e
Enzo Facundo Dos Santos <enzodossantos364@gmail.com>
2024-03-07 16:02:05 -0300

    feat(auth): Add support to auth factory

--- app/Http/Services/AuthService.php
+++ app/Http/Services/AuthService.php
@@ -2,33 +2,25 @@
 
 namespace App\Http\Services;
 
-use Exception;
-use App\Models\User;
-use Illuminate\Auth\AuthenticationException;
-use Illuminate\Support\Facades\Hash;
+use App\Http\Helpers\CredentialsValidatorFactory;
+use App\Http\Helpers\SessionTokenGenerator;
 
 class AuthService
 {
-    public function validateUserCredentials(array $credentials): string
+    private $credentialsValidatorFactory;
+    private $sessionTokenGenerator;
+
+    public function __construct(CredentialsValidatorFactory $credentialsValidatorFactory, SessionTokenGenerator $sessionTokenGenerator)
     {
-        $user = User::where('email', $credentials['email'])
-                    ->first();
-
-        if(!isset($user) || !Hash::check($credentials['password'], $user->password)){
-            throw new Exception('Invalid credentials.', 401);
-        }
-
-        return $this->createSessionToken($user);
+        $this->credentialsValidatorFactory = $credentialsValidatorFactory;
+        $this->sessionTokenGenerator = $sessionTokenGenerator;
     }
 
-    private function createSessionToken(User $user): string
+    public function createUserSession(string $method, array $credentials): string
     {
-        $user->tokens()->delete();
+        $validator = $this->credentialsValidatorFactory->makeValidator($method);
+        $user = $validator->validateCredentials($credentials);
 
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        $authToken = explode('|', $token)[1];
-
-        return $authToken;
+        return $this->sessionTokenGenerator->createSessionToken($user);
     }
 }
