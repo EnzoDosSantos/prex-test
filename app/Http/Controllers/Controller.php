8182773c23c2c90576a8fc02ad319db1f79ffdc9
Enzo Facundo Dos Santos <enzodossantos364@gmail.com>
2024-03-06 22:39:51 -0300

    chore(exceptions): Add base handle exception on base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    protected function handleException(Exception $exception): JsonResponse
+    {
+        $code = $exception->getCode();
+
+        if($code === 400){
+            $error = json_decode($exception->getMessage());
+        } else {
+            $error = $exception->getMessage();
+        }
+
+        return response()->json(['error' => $error], $code);
+    }
 }
