b4e3d199ae550284782a88cd04b899afafa7a9de
Enzo Facundo Dos Santos <enzodossantos364@gmail.com>
2024-03-06 22:40:23 -0300

    feat(auth): Add create session endpoint

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Utils\RequestValidator;
+use Exception;
+use Illuminate\Http\Request;
+use App\Http\Services\AuthService;
+use Illuminate\Http\JsonResponse;
+
+class AuthController extends Controller
+{
+    protected $authService;
+
+    public function __construct(AuthService $authService)
+    {
+        $this->authService = $authService;
+    }
+
+    public function createSession(Request $request): JsonResponse
+    {
+        try {
+            new RequestValidator($request, 'createSession');
+
+            $credentials = $request->only(['email', 'password']);
+
+            $token = $this->authService->validateUserCredentials($credentials);
+
+            return response()->json(['token' => $token], 200);
+        } catch (Exception $e) {
+            return $this->handleException($e);
+        }
+    }
+}
