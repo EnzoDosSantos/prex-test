c70f8f2fae1970ddc94c4a06efbf8682a820a58d
Enzo Facundo Dos Santos <enzodossantos364@gmail.com>
2024-03-07 18:01:52 -0300

    chore(logs): Add usage of logger on users endpoints

---
+++ app/Http/Middleware/LogApi.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\Models\LogApp;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LogApi
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+
+    public function handle(Request $request, Closure $next, string $service)
+    {
+        $request->merge(['service' => $service]);
+
+        return $next($request);
+    }
+
+    public function terminate($request, $response)
+    {
+        $user = Auth::user();
+
+        if(!isset($user)){
+            return;
+        }
+
+        $requestService = $request->service ?? 'UNKNOWN';
+
+        LogApp::create([
+            'user_id' => $user->id,
+            'service' => $requestService,
+            'request' => $request->getContent(),
+            'response' => $response->getContent(),
+            'endpoint' => $request->fullUrl(),
+            'status' => $response->status(),
+            'method' => $request->getMethod(),
+            'ip' => $this->realIp(),
+        ]);
+
+        return;
+    }
+
+    private function realIp()
+    {
+        $ip = $_SERVER['REMOTE_ADDR'];
+            if (isset($_SERVER['HTTP_X_FORWARDED_FOR']) && preg_match_all('#\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}#s', $_SERVER['HTTP_X_FORWARDED_FOR'], $matches)) {
+                foreach ($matches[0] AS $xip) {
+                    if (!preg_match('#^(10|172\.16|192\.168)\.#', $xip)) {
+                        $ip = $xip;
+                        break;
+                    }
+                }
+            } elseif (isset($_SERVER['HTTP_CLIENT_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_CLIENT_IP'])) {
+                $ip = $_SERVER['HTTP_CLIENT_IP'];
+            } elseif (isset($_SERVER['HTTP_CF_CONNECTING_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_CF_CONNECTING_IP'])) {
+                $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+            } elseif (isset($_SERVER['HTTP_X_REAL_IP']) && preg_match('/^([0-9]{1,3}\.){3}[0-9]{1,3}$/', $_SERVER['HTTP_X_REAL_IP'])) {
+                $ip = $_SERVER['HTTP_X_REAL_IP'];
+            }
+
+        return $ip;
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,6 @@ Route::prefix('auth')->group(function(){
 
 
 Route::prefix('users')->middleware('auth:sanctum')->group(function(){
-    Route::get('/gifts', [UserController::class, 'searchGifts']);
-    Route::get('/gift', [UserController::class, 'searchGift']);
+    Route::get('/gifts', [UserController::class, 'searchGifts'])->middleware('log:SEARCH_GIFTS');
+    Route::get('/gift', [UserController::class, 'searchGift'])->middleware('log:SEARCH_GIFT');
 });
